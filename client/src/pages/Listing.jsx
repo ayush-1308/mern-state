61258907906a6282e57fa485c691ec4bb139034f
ayush-1308 <ayushsinghlts123@gmail.com>
2024-02-15 21:23:26 +0530

    mini changes

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -90,8 +90,8 @@ export default function Listing() {
             <p className='text-2xl font-semibold'>
               {listing.name} - ₹{' '}
               {listing.offer
-                ? listing.discountPrice.toLocaleString('en-US')
-                : listing.regularPrice.toLocaleString('en-US')}
+                ? listing.regularPrice.toLocaleString('en-US')
+                : listing.discountPrice.toLocaleString('en-US')}
               {listing.type === 'rent' && ' / month'}
             </p>
             <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
@@ -104,7 +104,7 @@ export default function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice} OFF
+                  ${+listing.regularPrice - +listing.discountPrice} off
                 </p>
               )}
             </div>
